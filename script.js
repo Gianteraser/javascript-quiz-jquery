ec1f4657e3b901c00e52d073a8fabcaad16ad12e
Gianteraser <itsdavidthai@gmail.com>
2016-06-01 01:05:52 -0700

    Working quiz
    Added:
    Multiple question and answers
    final results page
    Bugs:
    correct answer counter is off

--- script.js
+++ script.js
@@ -45,37 +45,66 @@ var rightAnswerCounter = 0;
 // current right answer in number form
 var numericAnswer = arrayOfQuizQA[currArr]['correctAnswer']
 var answer = arrayOfQuizQA[currArr]['choices'][numericAnswer];
+//while loop counter
+var n = 0;
 
 
 
-// add question html
-$('body').append(question);
+// function to hold the quiz
 
-// add a input form the the html
-$('body').append(formDIV);
-
-
-// go through choice array adding each choice to a html input element
-for(var i =0; i < arrayOfQuizQA[currArr]['choices'].length; i++){
-
-	$('form').append("<input type='radio' name='choices' value="+ arrayOfQuizQA[currArr]['choices'][i]  +">" + arrayOfQuizQA[currArr]['choices'][i] + "</input>");
-
-
-}	
-
-// create button to advance to next question
-
-$('body').append(nextQuestionButton)
-
-
-
-// this will increment the answer counter if the right answer is selected
-// STILL NEEDS TO BE IMPLEMENTED- if a person goes back and forth selecting things will increment it more than once
-$('input').on('click', function(){
-	if(answer == $("input:checked").val()){
-		rightAnswerCounter++
+var quizMaker = function(){
+	//results page after all questions are answered
+	if(currArr >= arrayOfQuizQA.length){
+		$('body').append('<div id="quizBody"> </div>')
+		$('#quizBody').append("<h1>Congratulations you have completed the quiz!</h1>")
+		$('#quizBody').append('<div id="result">' + 'you answered ' + rightAnswerCounter + ' questions correctly' + ' </div>');
 
 	}
 
-})
+	//starting div
+	$('body').append('<div id="quizBody"> </div>')
 
+	// add question html
+	$('#quizBody').append('<div>' + arrayOfQuizQA[currArr]['question'] + '</div>');
+
+	// add a input form the the html
+	$('#quizBody').append(formDIV);
+
+
+	// go through choice array adding each choice to a html input element
+	for(var i =0; i < arrayOfQuizQA[currArr]['choices'].length; i++){
+
+		$('form').append("<input type='radio' name='choices' value="+ arrayOfQuizQA[currArr]['choices'][i]  +">" + arrayOfQuizQA[currArr]['choices'][i] + "</input>");
+
+
+	}	
+
+
+
+	// this will increment the answer counter if the right answer is selected
+	// STILL NEEDS TO BE IMPLEMENTED- if a person goes back and forth selecting things will increment it more than once
+	$('input').on('click', function(){
+		if(answer == $("input:checked").val()){
+			rightAnswerCounter++
+
+		}
+
+	})
+
+	// create button to advance to next question
+
+	$('#quizBody').append(nextQuestionButton)
+
+	$('input#next').click(function(){
+		$('#quizBody').remove()
+		currArr++;
+		quizMaker();
+		
+		
+
+	})
+
+
+}
+
+quizMaker();
